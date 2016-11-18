140a9bc5e29ccad5c1a8b836067964250e8d6f06
nikklein <objcoding@gmail.com>
2016-11-18 15:44:14 +0000

    add print error if incorrect time format has been used

--- lib/book_a_doc.rb
+++ lib/book_a_doc.rb
@@ -1,2 +1,3 @@
 abort("In order to book an appointment please specify the time.\n") if ARGV.empty?
 abort("Please use correct time format, e.g, 12:40 or 2.30\n") unless ARGV.first =~ /^(1[0-2]|0?[1-9]):([0-5][0-9])$/
+git

22f997e421a1456b9a8b064c5219c6688016116c
nikklein <objcoding@gmail.com>
2016-11-19 16:43:48 +0000

    pass tests

--- lib/book_a_doc.rb
+++ lib/book_a_doc.rb
@@ -3,7 +3,7 @@ def time_in_range?(time)
   working_hours.any? { |range| range.cover? time }
 end
 preffered_time = ARGV.first
-time_format = /^(1[0-2]|0?[1-9]):([0-5][0-9])$/
+time_format = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/
 abort("In order to book an appointment please specify the time.\n") if ARGV.empty?
-abort("Please use correct time format, e.g, 12:40 or 2.30\n") unless preffered_time =~ time_format
-abort("Appointments are avaialble between 08:00 and 3:00\n") unless time_in_range? preffered_time
+abort("Please use correct time format, e.g, 09:30 or 14:30\n") unless preffered_time =~ time_format
+abort("Appointments are avaialble between 08:00 and 15:00\n") unless time_in_range? preffered_time

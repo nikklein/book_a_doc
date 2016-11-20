48a99701a9e51012341b89e00739b95938cd16df
nikklein <objcoding@gmail.com>
2016-11-20 18:28:33 +0000

    update controller

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,10 +1,16 @@
 require_relative 'validations'
+require_relative 'booking'
+require_relative 'file_processor'
 
 time = ARGV.first
+path = ENV['FULL_PATH_TO_PROGRAM']
 
-def run_program(time)
+def run_program(time, path)
   validator = Validations.new(time)
   validator.validate_input
+  file_processor = FileProcessor.new(path)
+  booking = Booking.new(file_processor, time)
+  booking.book_a_time
 end
 
-run_program(time)
+run_program(time, path)

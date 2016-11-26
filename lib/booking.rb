981570a53e76f8d51e66a9eb71706aa773e923e4
nikklein <objcoding@gmail.com>
2016-11-26 19:17:17 +0000

    refactor; remove booking_spec

--- lib/booking.rb
+++ lib/booking.rb
@@ -5,43 +5,34 @@ class Booking
   end
 
   def book_a_doc
-    available_slots = @file_processor.to_hash
-    slots = available_slots['availability_slots']
-    slots_empty?(slots)
-    find_time = slots.index slots.detect { |el| el['time'] == @time }
-
-    if find_time.nil?
-      index = find_next_slot_index(slots)
-      print_result(index, slots)
-      slots.slice!(index, 1)
-      update_file(available_slots)
-    else
-      print_result(find_time, slots)
-      slots.slice!(find_time, 1)
-      update_file(available_slots)
-    end
-  end
-
-  def find_next_slot_index(slots)
-    time_slots = []
-    slots.each { |el| time_slots << el['time'] }
-    time_slots << @time
-    sorted = time_slots.sort
-    check_next_index(sorted)
-    sorted.index @time
-  end
-
-  def slots_empty?(slots)
     abort('No times are available') if slots.empty?
+    index = find_slot(available_slots)
+    abort('No times are currently available!') if index.nil?
+    print_result(index, available_slots)
+    update_slots(index, available_slots)
   end
 
-  def check_next_index(time_slots)
-    abort('No times are currently available!') if time_slots.last == @time
+  def update_slots(find_slot, available_slots)
+    available_slots = available_slots
+    slots.slice!(find_slot, 1)
+    update_file(available_slots)
+  end
+
+  def find_slot(_available_slots)
+    slots.index slots.detect { |el| el['time'] == @time || el['time'] > @time }
   end
 
   private
 
-  def print_result(index, slots)
+  def available_slots
+    @file_processor.to_hash
+  end
+
+  def slots
+    available_slots['availability_slots']
+  end
+
+  def print_result(index, _slots)
     puts format_for_print(slots[index]['time'])
   end
 
@@ -50,7 +41,7 @@ class Booking
   end
 
   def format_time(time)
-    time[-1] == '0' ? @time = time << ':00' : @time = time.split(':').first << ':' << ((time.split(':').last.to_i / 10.0).ceil * 10).to_s << ':00'
+    @time = time << ':00'
   end
 
   def format_for_print(time)
--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -1,23 +1,23 @@
-require './lib/booking'
-
-describe Booking do
-  describe '#find_next_slot_index' do
-    it 'finds an index of the next slot' do
-      anything = double('anything')
-      booking = Booking.new(anything, '08:30')
-      slot = [{ 'time' => '08:40:00' }]
-      expect(booking.find_next_slot_index(slot)).to eq(0)
-    end
-  end
-  describe '#check_next_index' do
-    it 'checks index of the next slot' do
-      booking = Booking.new('/', '08:30')
-      time_slots = ['08:30:00']
-      begin
-        booking.check_next_index(time_slots)
-      rescue SystemExit => e
-        expect(e.status).to eq(1)
-      end
-    end
-  end
-end
+# require './lib/booking'
+#
+# describe Booking do
+#   describe '#find_next_slot_index' do
+#     it 'finds an index of the next slot' do
+#       anything = double('anything')
+#       booking = Booking.new(anything, '08:30')
+#       slot = [{ 'time' => '08:40:00' }]
+#       expect(booking.find_next_slot_index(slot)).to eq(0)
+#     end
+#   end
+#   # describe '#check_next_index' do
+#   #   it 'checks index of the next slot' do
+#   #     booking = Booking.new('/', '08:30')
+#   #     time_slots = ['08:30:00']
+#   #     begin
+#   #       booking.check_next_index(time_slots)
+#   #     rescue SystemExit => e
+#   #       expect(e.status).to eq(1)
+#   #     end
+#   #   end
+#   # end
+# end
